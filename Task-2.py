881086a396d3d813a2f4984979898971a2ea6dfe
indal15385-cmd <indal15385@gmail.com>
2025-09-20 21:36:45 +0530

    Add files via upload

---
+++ Task-2.py
@@ -0,0 +1,4 @@
+First_Name= input("Enter your first Name: ")
+Last_Name = input("Enter your Second Name: ")
+Full_Name = First_Name+" "+Last_Name
+print("Hello"+" "+Full_Name+"!"+" "+"Welcome to Python world")
